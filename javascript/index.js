203959334cebc9a92fbf046c47291ea62e6eab8f
Bruna Lima Farias <burunalfarias@gmail.com>
2024-06-30 18:32:38 -0300

    Estrutura e logica

---
+++ javascript/index.js
@@ -0,0 +1,39 @@
+function Somar(){
+
+    var input1 = parseInt(document.getElementById("input1").value);
+    var input2 = parseInt(document.getElementById("input2").value);
+    total = input1 + input2;
+
+    alert(total);
+       
+}
+
+function Subtrair(){
+
+    var input1 = parseInt(document.getElementById("input1").value);
+    var input2 = parseInt(document.getElementById("input2").value);
+    total = input1 - input2;
+
+    alert(total);
+       
+}
+
+function Multiplicar(){
+
+    var input1 = parseInt(document.getElementById("input1").value);
+    var input2 = parseInt(document.getElementById("input2").value);
+    total = input1 * input2;
+
+    alert(total);
+       
+}
+
+function Dividir(){
+
+    var input1 = parseInt(document.getElementById("input1").value);
+    var input2 = parseInt(document.getElementById("input2").value);
+    total = input1 / input2;
+
+    alert(total);
+       
+}
